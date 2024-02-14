ce2859ded85799dd4685499a1aaf890c85147f38
vlakoosh <vlakoosh@gmail.com>
2024-02-14 18:49:43 +0100

    the kg/lbs selectors are working now
    fixed html radio buttons
    
    added bmi calculations and display
    
    added proper rounding to bmi (1decimal

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const heightButtonCm = document.querySelector('#radioHeightCm');
 const heightButtonFt = document.querySelector('#radioHeightFt');
+
+const weightButtonKg = document.querySelector('#radioWeightKg');
+const weightButtonLbs = document.querySelector('#radioWeightLbs');
+
 const submitButton = document.querySelector("#submitButton");
 
 let heightMode = "cm"
+let weightMode = "kg"
 
 heightButtonCm.addEventListener('click', () => {
     heightMode = "cm"
@@ -16,6 +21,19 @@ heightButtonFt.addEventListener('click', () => {
         '<input type="number" class="in" name="" id="heightIn" placeholder="height (in)">';
 });
 
+weightButtonKg.addEventListener('click', () => {
+    weightMode = "kg";
+    const weightInputContainer = document.querySelector('.container-weight');
+    weightInputContainer.innerHTML = '<input type="number" name="" id="weight" placeholder="weight (kg)">';
+});
+
+weightButtonLbs.addEventListener('click', () => {
+    weightMode = "lbs";
+    const weightInputContainer = document.querySelector('.container-weight');
+    weightInputContainer.innerHTML = '<input type="number" name="" id="weight" placeholder="weight (lbs)">';
+});
+
+
 submitButton.addEventListener('click', () => {
     const resultText = document.querySelector('.result-text');
     try {
@@ -23,15 +41,31 @@ submitButton.addEventListener('click', () => {
         if (heightMode === "cm"){
             const cmInput = document.querySelector('#height')
             heightCm = parseInt(cmInput.value);
-            console.log(heightCm)
+            console.log(heightCm + "cm")
         }
         else {
             const ftInput = document.querySelector('#heightFt')
             const inInput = document.querySelector('#heightIn')
             heightCm = (parseInt(ftInput.value) * 12 + parseInt(inInput.value)) * 2.54;
-            console.log(heightCm)
+            console.log(heightCm + "cm")
         }
+
+        let weightKg = 0;
+        if (weightMode === "kg") {
+            const weightInput = document.querySelector('#weight');
+            weightKg = parseInt(weightInput.value);
+            console.log(weightKg + "kg")
+        }
+        else {
+            const weightInput = document.querySelector('#weight');
+            weightKg = parseInt(weightInput.value) * 2.20462;
+            console.log(weightKg + "kg")
+        }
+
+        const bmi = Math.round((weightKg / Math.pow(heightCm/100, 2)) * 10) / 10;
+        resultText.textContent = "BMI: " + bmi;
     } catch (e) {
         console.log(e);
     }
+
 });
